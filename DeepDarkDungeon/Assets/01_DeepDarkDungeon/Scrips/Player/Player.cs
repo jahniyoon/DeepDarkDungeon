f1c9ca72f7f9dad3fe8664ce0c256107338f063d
DESKTOP-NRV9GE8\jinyoungsoo <rokjinyoungsoo@gmail.com>
2023-08-03 16:42:20 +0900

    [jinyoungsoo] Add playerScript Update

--- DeepDarkDungeon/Assets/01_DeepDarkDungeon/Scrips/Player/Player.cs
+++ DeepDarkDungeon/Assets/01_DeepDarkDungeon/Scrips/Player/Player.cs
@@ -6,10 +6,16 @@ public class Player : MonoBehaviour
 {
     public float speed = default; // speed 를 default로 수정 : 지환
 
+    Rigidbody PlayerRigid;     //리지드 바디 넣음 : 영수
+    Animator animator;        //애니메이터 넣음 : 영수
+
     // Start is called before the first frame update
     void Start()
     {
         // 지환 수정함;
+
+        PlayerRigid = GetComponent<Rigidbody>();    //리지드 불러옴 : 진
+        animator = GetComponent<Animator>();        //애니메이터 불러옴 : 진
     }
 
     // Update is called once per frame
