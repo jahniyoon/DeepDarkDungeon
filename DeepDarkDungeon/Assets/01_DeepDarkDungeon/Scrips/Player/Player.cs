c14283c7d5e4dbca99a17f7a4cf8501a85a256bf
DESKTOP-484M8JG\KGA <jahniyoon@gmail.com>
2023-08-03 16:18:54 +0900

    [Jihwan] 지환이 player 스크립트 수정

--- DeepDarkDungeon/Assets/01_DeepDarkDungeon/Scrips/Player/Player.cs
+++ DeepDarkDungeon/Assets/01_DeepDarkDungeon/Scrips/Player/Player.cs
@@ -7,7 +7,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // 지환 수정함;
     }
 
     // Update is called once per frame
