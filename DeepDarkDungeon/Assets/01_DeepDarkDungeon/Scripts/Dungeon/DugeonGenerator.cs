db46c46854e7e213beecc870a3de7c8ce64989fb
Jahni <32658397+jahniyoon@users.noreply.github.com>
2023-08-07 00:45:32 +0900

    [jahni] 복도생성 연구중...2

--- DeepDarkDungeon/Assets/01_DeepDarkDungeon/Scripts/Dungeon/DugeonGenerator.cs
+++ DeepDarkDungeon/Assets/01_DeepDarkDungeon/Scripts/Dungeon/DugeonGenerator.cs
@@ -4,8 +4,8 @@ using System.Linq;
 using UnityEngine;
 public class DugeonGenerator
 {
- // 던전 만들기
- 
+    // 던전 만들기
+
     List<RoomNode> allNodesCollection = new List<RoomNode>();
     private int dungeonWidth;
     private int dungeonLength;
@@ -20,6 +20,54 @@ public class DugeonGenerator
         this.dungeonLength = dungeonLength;
     }
 
+    //// 방이 바닥과 겹치는지 확인
+    //public List<Node> CalculateDungeon(int maxIterations, int roomWidthMin, int roomLengthMin, float roomBottomCornerModifier, float roomTopCornerMidifier, int roomOffset, int corridorWidth)
+    //{
+    //    BinarySpacePartitioner bsp = new BinarySpacePartitioner(dungeonWidth, dungeonLength);
+    //    allNodesCollection = bsp.PrepareNodesCollection(maxIterations, roomWidthMin, roomLengthMin);
+    //    List<Node> roomSpaces = StructureHelper.TraverseGraphToExtractLowestLeafes(bsp.RootNode);
+
+    //    RoomGenerator roomGenerator = new RoomGenerator(maxIterations, roomLengthMin, roomWidthMin);
+
+    //    while (true)
+    //    {
+    //        CreatedRooms = roomGenerator.GenerateRoomsInGivenSpaces(roomSpaces, roomBottomCornerModifier, roomTopCornerMidifier, roomOffset);
+    //        CorridorsGenerator corridorGenerator = new CorridorsGenerator();
+    //        var corridorList = corridorGenerator.CreateCorridor(allNodesCollection, corridorWidth);
+
+    //        // Check for overlap between rooms and corridors
+    //        bool hasOverlap = CheckRoomCorridorOverlap(CreatedRooms, corridorList);
+
+    //        if (!hasOverlap)
+    //        {
+    //            return new List<Node>(CreatedRooms).Concat(corridorList).ToList();
+    //        }
+    //    }
+    //}
+
+    //private bool CheckRoomCorridorOverlap(List<RoomNode> rooms, List<Node> corridors)
+    //{
+    //    foreach (var corridor in corridors)
+    //    {
+    //        foreach (var room in rooms)
+    //        {
+    //            if (DoRectanglesOverlap(corridor.BottomLeftAreaCorner, corridor.TopRightAreaCorner,
+    //                                    room.BottomLeftAreaCorner, room.TopRightAreaCorner))
+    //            {
+    //                return true; // Overlap detected
+    //            }
+    //        }
+    //    }
+    //    return false; // No overlap
+    //}
+
+    //private bool DoRectanglesOverlap(Vector2Int rect1Min, Vector2Int rect1Max, Vector2Int rect2Min, Vector2Int rect2Max)
+    //{
+    //    return rect1Min.x < rect2Max.x && rect1Max.x > rect2Min.x &&
+    //           rect1Min.y < rect2Max.y && rect1Max.y > rect2Min.y;
+    //}
+
+    //방 체크 코드 추가
     public List<Node> CalculateDungeon(int maxIterations, int roomWidthMin, int roomLengthMin, float roomBottomCornerModifier, float roomTopCornerMidifier, int roomOffset, int corridorWidth)
     {
         BinarySpacePartitioner bsp = new BinarySpacePartitioner(dungeonWidth, dungeonLength);
@@ -35,6 +83,7 @@ public class DugeonGenerator
         return new List<Node>(CreatedRooms).Concat(corridorList).ToList();
     }
 
+    // 기존 코드
     //public List<Node> CalculateDungeon(int maxIterations, int roomWidthMin, int roomLengthMin, float roomBottomCornerModifier, float roomTopCornerMidifier, int roomOffset, int corridorWidth)
     //{
     //    BinarySpacePartitioner bsp = new BinarySpacePartitioner(dungeonWidth, dungeonLength);
