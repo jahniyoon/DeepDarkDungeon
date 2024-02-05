4647f954b402891d8ed044aa9a5fa9975dea45f4
Jihwan <jahniyoon@gmail.com>
2024-02-05 15:50:49 +0900

    윤지환 [Add] 렌더 피쳐를 활용한 벽 뒤 실루엣 처리

--- DeepDarkDungeon/Assets/01_DeepDarkDungeon/Shader/Scripts/RendererFeatureController.cs
+++ DeepDarkDungeon/Assets/01_DeepDarkDungeon/Shader/Scripts/RendererFeatureController.cs
@@ -5,13 +5,6 @@ using UnityEngine.Rendering.Universal;
 using UnityEngine.Rendering;
 using System.Reflection;
 
-[System.Serializable]
-public struct RenderFeatureToggle
-{
-    public ScriptableRendererFeature feature;
-    public bool isEnabled;
-}
-
 // ExecuteAlways : 에디트 모드 or 플레이 모드일 때 무언가 하기 위한 어트리뷰트
 [ExecuteAlways]
 public class RendererFeatureController : MonoBehaviour
@@ -19,34 +12,40 @@ public class RendererFeatureController : MonoBehaviour
     [SerializeField]
     private UniversalRenderPipelineAsset pipelineAsset;     // 현재 사용중인 렌더파이프라인 에셋
     [SerializeField]
-    private List<RenderFeatureToggle> renderFeatures = new List<RenderFeatureToggle>();
+    private List<ScriptableRendererFeature> rendererFeatures = new List<ScriptableRendererFeature>();
+
+    public string[] defaultRendererSettings;
 
     private void Start()
     {
         pipelineAsset = (UniversalRenderPipelineAsset)GraphicsSettings.renderPipelineAsset;
-        renderFeatures.Clear();
+        rendererFeatures.Clear();
 
         FieldInfo fieldInfo = pipelineAsset.GetType().GetField("m_RendererDataList", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
         ScriptableRendererData[] rendererDatas = (ScriptableRendererData[])fieldInfo.GetValue(pipelineAsset);
         
         foreach (ScriptableRendererFeature rendererFeature in rendererDatas[0].rendererFeatures)
         {
-            RenderFeatureToggle renderFeature;
-            renderFeature.feature = rendererFeature;
-            renderFeature.isEnabled = rendererFeature.isActive;
-            renderFeatures.Add(renderFeature);
+            ScriptableRendererFeature renderFeature;
+            renderFeature = rendererFeature;
+            rendererFeature.SetActive(false);
+            rendererFeatures.Add(renderFeature);
         }
-        
 
-    }
-
-
-    private void Update()
-    {
-        foreach (RenderFeatureToggle toggleObj in renderFeatures)
+        foreach(string item in defaultRendererSettings)
         {
-            toggleObj.feature.SetActive(toggleObj.isEnabled);
+            SetRenderer(item);
         }
     }
+
+    // 렌더러 세팅
+    public void SetRenderer(string name)
+    {
+        ScriptableRendererFeature rendererFeature = rendererFeatures.Find(item => item.name.Equals(name));
+        rendererFeature.SetActive(!rendererFeature.isActive);     
+    }
+
+
+  
 
 }
